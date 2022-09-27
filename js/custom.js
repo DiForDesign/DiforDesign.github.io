62560e9ea819804b327fc2d3eb47eb5bcecade63
DiForDesign <DiForDesign@users.noreply.github.com>
2022-09-27 17:03:16 +0800

    Index banner redesign

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+function scrollToElement(x) {
+        let e = document.getElementById(x);
+        e.scrollIntoView({
+          //block: 'start',
+          behavior: 'smooth',
+          //inline: 'start'
+        });
+      }
